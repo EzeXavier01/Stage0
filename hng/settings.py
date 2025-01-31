4c656c4fc9a92bee6dc412811bc94e87dd3020ed
EzeXavier01 <ezexavier103@gmail.com>
2025-01-31 09:53:45 +0100

    Hosts

--- hng/settings.py
+++ hng/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("stage0")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',  ".onrender.com"]
 
 
 # Application definition
