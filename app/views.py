3c90167d4a8104645354281618438908c306fff2
EzeXavier01 <ezexavier103@gmail.com>
2025-01-31 10:29:24 +0100

    Deploy

--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
-from django.http import JsonResponse
-from datetime import datetime
+from django.http import JsonResponse # type: ignore
+from datetime import datetime, timezone
 
 def info(request):
     data = {
         'email': 'ezeifeanyichukwu103@gmail.com',
-        'current_datetime': datetime.now().isoformat(),
+        'current_datetime':  datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
         'github_url': 'https://github.com/EzeXavier01/Stage0'
     }
     return JsonResponse(data)
